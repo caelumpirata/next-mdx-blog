fb1277e114e905ea7b040785ebf00eb51d101278
Caelum Pirata <caelumpirata@gmail.com>
2024-08-16 14:57:01 +0530

    basic route fixed but without /blog/blog-name

---
+++ app/posts.ts
@@ -0,0 +1,28 @@
+import { readdir } from "fs/promises";
+
+export interface Post {
+    slug: string;
+    title: string;
+    publishDate: string;
+  }
+
+  
+export async function getPosts(): Promise<Post[]> {
+    // Retreive slugs from post routes
+    const slugs = (
+      await readdir("./app/(posts)", { withFileTypes: true })
+    ).filter((dirent) => dirent.isDirectory());
+  
+    // Retreive metadata from MDX files
+    const posts = await Promise.all(
+      slugs.map(async ({ name }) => {
+        const { metadata } = await import(`/app/(posts)/${name}/page.mdx`);
+        return { slug: name, ...metadata };
+      })
+    );
+  
+    // Sort posts from newest to oldest
+    posts.sort((a, b) => +new Date(b.publishDate) - +new Date(a.publishDate));
+  
+    return posts;
+  }
---
+++ components/posts/index.tsx
@@ -0,0 +1,20 @@
+import { Post } from '@/app/posts';
+import Link from 'next/link';
+
+export function Posts({ posts }: { posts: Post[] }) {
+  return (
+    <ol>
+      {posts.map(({ slug, title, publishDate }) => (
+        <li key={slug}>
+          <h2>
+            <Link href={`/${slug}`}>{title}</Link>
+          </h2>
+          <p>
+            <strong>Published:</strong>{' '}
+            {new Date(publishDate).toLocaleDateString()}{' '}
+          </p>
+        </li>
+      ))}
+    </ol>
+  );
+}
