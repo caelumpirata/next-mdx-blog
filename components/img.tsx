c8666266308b176df4949d2312dbf70b69b76df0
Caelum Pirata <caelumpirata@gmail.com>
2024-08-20 15:24:43 +0530

    fix images error in mdx components

--- components/img.tsx
+++ components/img.tsx
@@ -1,40 +1,20 @@
-// import { type ComponentPropsWithoutRef } from 'react';
-// import Image from 'next/image';
-
-// export function Img(props: ComponentPropsWithoutRef<'img'>) {
-//   return (
-    
-    
-// <Image
-//   src="/images/featured-image.jpg"
-//   width={768}
-//   height={500}
-//   alt="Picture of the author"
-//   className="my-5 flex flex-col items-center rounded-3xl"
-// />
-
-
-//   )
-// }
-
-
 import { type ComponentPropsWithoutRef } from 'react';
 import Image from 'next/image';
 
-interface ImgProps extends Omit<ComponentPropsWithoutRef<'img'>, 'width' | 'height'> {
-  width?: number;
-  height?: number;
-}
+type ImgProps = ComponentPropsWithoutRef<'img'>;
 
-export function Img({ src, width = 768, height = 500, alt = 'Image', className, ...rest }: ImgProps) {
+export function Img({ src, alt, width = 768, height = 500, ...props }: ImgProps) {
+  // Extract the actual src path from the Markdown-style syntax
+  const srcPath = typeof src === 'string' ? (src.match(/\((.*?)\)/)?.[1] || src) : '';
+
   return (
     <Image
-      src={src || '/images/featured-image.jpg'}  // Use default image if src is not provided
-      width={typeof width === 'number' ? width : 768}  // Ensure width is a number
-      height={typeof height === 'number' ? height : 500}  // Ensure height is a number
-      alt={alt}
-      className={`my-5 flex flex-col items-center rounded-3xl ${className || ''}`}  // Ensure className is a string
-      {...rest}  // Spread remaining props
+      src={srcPath}
+      width={Number(width) || 768}
+      height={Number(height) || 500}
+      alt={alt || ''}
+      className="my-5 flex flex-col items-center rounded-3xl"
+      {...props}
     />
   );
 }
